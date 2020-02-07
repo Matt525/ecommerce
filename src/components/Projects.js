e343df57f1f139761f0305d0bb4b8d8c86bdc49b
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2020-02-06 22:28:36 -0500

    projects section change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,117 +1,82 @@
 import React, { Component } from "react";
 import "./projects.scss";
+import {
+  Card, Button, CardImg, CardTitle, CardText, CardDeck,
+  CardSubtitle, CardBody, Container, Row, Col
+} from 'reactstrap';
+
+
+
+
 
 class Projects extends Component {
   render() {
     return (
+
+      
       <div>
         <div className="section-title">
           <h1 id="projects-title">P R O J E C T S</h1>
         </div>
 
         {/* <!-- One --> */}
-        <section id="one" class="tiles">
-          <article>
-            <div className="overlay"></div>
-            <span class="image">
-              <img src="" alt="" />
-            </span>
-  
-            <header class="major">
-              <h3>
-                <a
-                  href="https://matt525.github.io/patatapclone/"
-                  class="link"
-                  style={{ fontWeight: "800" }}
-                >
-                  Patatap Clone
-                </a>
-              </h3>
-              <p style={{ fontSize: "25px" }}>
-                Web application using javascript, and css to trigger audio
-                samples and animations upon the press of a key.
-              </p>
-              <button class="btn blue" style={{ margin: "10px" }} href="https://matt525.github.io/patatapclone/">
-                Demo
-              </button>
-              <button class="btn">Code</button>
-            </header>
-          </article>
-          <article>
-            <span class="image">
-              <img src="" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a
-                  href="landing.html"
-                  class="link"
-                  style={{ fontWeight: "800"}}
-                >
-                  RGB Color Game
-                </a>
-              </h3>
-              <p style={{ fontSize: "25px" }}>
-                Used vanilla JS to create a rgb color guessing game.
-              </p>
-              <button class="btn blue btn-link" style={{ margin: "10px" }}>
-                Demo
-              </button>
-              <button class="btn btn-link">Code</button>
-            </header>
-          </article>
-
-          <article>
-            <span class="image">
-              <img src="" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a
-                  href="landing.html"
-                  class="link"
-                  style={{ fontWeight: "800" }}
-                >
-                  B R E A K O U T
-                </a>
-              </h3>
-              <p style={{ fontSize: "25px" }}>
-                Uses vanilla javascript to render and control breakout game
-                elements
-              </p>
-              <button class="btn blue" style={{ margin: "10px" }}>
-                Demo
-              </button>
-              <button class="btn">Code</button>
-            </header>
-          </article>
-
-          <article>
-            <span class="image">
-              <img src="images/pic04.jpg" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a
-                  href="landing.html"
-                  class="link"
-                  style={{ fontWeight: "800" }}
-                >
-                  Weather Application
-                </a>
-              </h3>
-              <p style={{ fontSize: "25px" }}>
-                Styled application using openWeather API to display current
-                weather data
-              </p>
-              <button class="btn blue" style={{ margin: "10px" }}>
-                Demo
-              </button>
-              <button class="btn">Code</button>
-            </header>
-          </article>
-        </section>
-      </div>
+      
+      
+      <CardDeck style={{background: '#222', margin: '20px'}}>
+        <Row>
+          <Col>
+      <Card height="100%">
+        <CardImg top width="100%" src="https://i.ytimg.com/vi/v3orh07cRPA/maxresdefault.jpg" alt="Card image cap" />
+        <CardBody style={{padding: '20px'}}>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <Button>Demo</Button>
+          <Button>Code</Button>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col>
+      <Card>
+        <CardImg top width="100%" src="https://codepen.io/jsjoeio/pen/WjvjaQ/image/large.png" alt="Card image cap" />
+        <CardBody style={{padding: '20px'}}>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Demo</Button>
+          <Button>Code</Button>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col>
+      <Card>
+        <CardImg top width="100%" src="https://mdn.mozillademos.org/files/10383/mdn-breakout-gameplay.png" alt="Card image cap" />
+        <CardBody style={{padding: '20px'}}>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+          <Button className="blue">Demo</Button>
+          <Button className="blue" style={{margin: "10px"}}>Code</Button>
+        </CardBody>
+      </Card>
+      </Col>
+      <Col>
+      <Card style={{width: '600px'}}>
+        <CardImg top width="100%"src="https://mdn.mozillademos.org/files/10383/mdn-breakout-gameplay.png" alt="Card image cap" />
+        <CardBody style={{padding: '20px'}}>
+          <CardTitle>Weather Application</CardTitle>
+          <CardSubtitle>Weather application that pulls from openweather api.</CardSubtitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+          <Button className="blue">Demo</Button>
+          <Button className="blue" style={{margin: "10px"}}>Code</Button>
+        </CardBody>
+      </Card>
+      </Col>
+      </Row>
+    </CardDeck>
+    
+    </div>
+      
     );
   }
 }
