ffcc2a40d5e942364d4f9160314ac57c6c7cd311
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2020-03-27 14:22:03 -0400

    weather link re-fix

---
+++ build/precache-manifest.533e88ed8af632c1b6d427f12411613a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f2d97814458c5438fe685d30936eff",
+    "url": "/matthewstaton.github.io/index.html"
+  },
+  {
+    "revision": "a58a6fa7a451089a3b2b",
+    "url": "/matthewstaton.github.io/static/css/main.c789780d.chunk.css"
+  },
+  {
+    "revision": "2961741b7f719275ab3f",
+    "url": "/matthewstaton.github.io/static/js/2.5ba5ad70.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/matthewstaton.github.io/static/js/2.5ba5ad70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58a6fa7a451089a3b2b",
+    "url": "/matthewstaton.github.io/static/js/main.2af24a1d.chunk.js"
+  },
+  {
+    "revision": "61dba22e65acb01e9a1c",
+    "url": "/matthewstaton.github.io/static/js/runtime-main.450f8d07.js"
+  },
+  {
+    "revision": "ff0e1f25e65ebd399b7c77cd3e5f0e81",
+    "url": "/matthewstaton.github.io/static/media/Profile_Pic.ff0e1f25.jpg"
+  }
+]);
